fddb0f12ff6837f32f190fab4b73db4979202e95
Jefferson <roacamacho1996@gmail.com>
2026-04-02 01:44:33 -0500

    committ

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app.include_router(webRouter.router)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "             ",
+        "https://diabetes-frontend-tbui.onrender.com",
     ],
     allow_methods=["POST", "GET"],
     allow_headers=["*"],
